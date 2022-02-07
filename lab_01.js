bf13917ba7c65d333f6d8fa2d7942c395115b7d5
Kian Aira <90256501+marquisKian@users.noreply.github.com>
2022-02-07 16:41:43 +0800

    removed while_loop in function

--- lab_01.js
+++ lab_01.js
@@ -1,27 +1,41 @@
 
-// const example_list = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1];
 
-//trying to put a random number generator into a 10 element list
-const array  = Array.from({length: 20}, () => Math.floor(Math.random() * 20));
-console.log(array);
+// const array = [5, 6, 7, 8, 9, 10];
+const element = 10
+const array = [];
+
+if(element == 0) {
+    console.log(null)
+}
+
+do {
+    const randomNumber = Math.floor(Math.random() * 100) + 1
+    if (!array.includes(randomNumber)) {
+        array.push(randomNumber);
+    }
+
+} while (array.length < element);
+console.log(array)
+//array of random numbers (not repeating)
+
 
 
 function insertionSort(array) {
-    const n = array.length;
-        for (let i = 1; i < n; i++) {
-            let current = array[i];
-            let j = i-1;
-            while((j > - 1) && (current < array[j])) {
-                array[j + 1] = array[j];
-                j--;
-            }
-            array[j+1] = current;
+    for (let i = 1; i < array.length; i++) {
+        for (let k = i; k > 0 && array[k] < array[k - 1]; k--) {
+            const temp = array[k]
+            array[k] = array[k - 1]
+            array[k - 1] = temp
         }
-    return array;
+    }
 }
 
+const start = Date.now()
 insertionSort(array);
+const end = Date.now()
 console.log(array);
+
+console.log('elapsed', (end - start) / 60_000)
 
 
 
